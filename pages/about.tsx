117c6c140cbd63edc6913fb195c574bc7b6b63ad
Nico <nickypellerin@gmail.com>
2020-06-25 16:27:03 -0400

    Fix typo

--- pages/about.tsx
+++ pages/about.tsx
@@ -48,8 +48,8 @@ const AboutPage = () => {
                 alt="Icon"
               />
               <Text>
-                Chattr is a free 1-to-1 P2P audio/video and chat platform built.
-                It provides a secure way of communicating on the web. File
+                Chattr is a free 1-to-1 P2P audio/video and chat platform. It
+                provides a secure way of communicating on the web. File
                 transfering (<strong>BETA</strong>) is also available, with
                 files up to 5MB. The project is still in its infancy and more
                 features will likely be added.
