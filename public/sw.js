06257ca95d0b80e7c5a5a7c607b58af4a0fd4d65
Nico <nickypellerin@gmail.com>
2020-06-29 20:59:21 -0400

    Fix lolz

--- public/sw.js
+++ public/sw.js
@@ -118,7 +118,7 @@ define("./sw.js",['./workbox-8666a044'], function (workbox) { 'use strict';
     "revision": "9d11ea7095b60a828f2db49ed1004ade"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "787529379f99c545d7cd00fbef2d469c"
+    "revision": "6bc0d9cedd4e045702e519498e6e3f22"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });
