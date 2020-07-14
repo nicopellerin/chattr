fbe0bc4f1ad57fb09d25732370f65f4103c74f16
Nico <nickypellerin@gmail.com>
2020-07-14 19:08:17 -0400

    Fixed height for messenger icon

--- components/Chat/Invite.tsx
+++ components/Chat/Invite.tsx
@@ -44,6 +44,8 @@ const Invite = () => {
           rel="noopener"
         >
           <FbookMessengerIcon
+            height="80"
+            width="80"
             whileHover={{ scale: 1.02 }}
             whileTap={{ scale: 0.98 }}
             src="/messenger.svg"
