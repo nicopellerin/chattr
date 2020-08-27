70252c7f7909a0e9fb4eafcad6c75e6df9e408f0
Nico <nickypellerin@gmail.com>
2020-08-27 18:22:46 -0400

    Optimize images

--- global.d.ts
+++ global.d.ts
@@ -2,6 +2,8 @@ declare module "better-react-spinkit"
 declare module "socket.io-encrypt"
 declare module "get-user-media-promise"
 
+declare module "*.webp"
+
 // type ShareData = {
 //   title?: string
 //   text?: string
